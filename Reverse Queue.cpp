810dac4eba05ddc96daab00e3f9df0830f0a1471
Ayush Nighot <68859871+AyushNighot@users.noreply.github.com>
2024-02-04 09:38:29 +0530

    Reverse Queue.cpp

---
+++ Reverse Queue.cpp
@@ -0,0 +1,20 @@
+#include<stack>
+#include <bits/stdc++.h> 
+queue<int> reverse(queue < int > & q) {
+    
+    stack<int>s;
+    while(!q.empty())
+    {
+        int element = q.front();
+        q.pop();
+        s.push(element);
+    }
+
+    while(!s.empty())
+    {
+        int  element = s.top();
+        s.pop();
+        q.push(element);
+    }
+    return q;
+}
